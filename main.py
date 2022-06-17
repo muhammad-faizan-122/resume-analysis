80f1fd06596b4faf45541a8d95ca9002ab7af0f8
Muhammad_Faizan_001 <faizanahmed2k18@gmail.com>
2022-06-17 17:20:43 +0500

    code refactoring

--- main.py
+++ main.py
@@ -9,17 +9,32 @@ import numpy as np
 # %matplotlib inline
 
 
-pdf_file_name = 'resume_pdf_files/Arslan_Arif_AI_2021.pdf'
-pdfReader = PdfReader(pdf_file_name) # Read file
-num_pages = len(pdfReader.pages) # Get total number of pages
+# Create dictionary with industrial and system engineering key terms by area
+terms = {'CV':['opencv', 'tensorflow', 
+               'image classification', 
+               'image segmentation', 
+               'object detection'],      
 
-# print("total number of pages in pdf file: ", num_pages)
+        'NLP':['nlp, ''transformer', 
+              'encoder decoder', 
+              'bert', 'text analysis',
+              'sentiment analysis',
+              'chatbot', 'pytorch', 
+              'NLTK', 'sequence model', 
+              'text mining'],
+
+        'Tradition ML':['python','matplotlib',
+                        'scikit learn', 'regression', 
+                        'numpy', 'pandas'],
+
+        'Other skills': ['python', 'git', 'docker']
+        }
 
 
-count = 0 # Initialize a count for the number of pages
-
-
-text = "" # Initialize a text empty string variable
+def read_input_pdf(fileName):
+    pdf_file_name = 'resume_pdf_files/Arslan_Arif_AI_2021.pdf'
+    pdfReader = PdfReader(pdf_file_name) # Read file
+    return pdfReader
 
 
 def average_calculator(opencv, nlp, trad_ml, others, terms):
@@ -29,115 +44,93 @@ def average_calculator(opencv, nlp, trad_ml, others, terms):
     others_avg = (others/len(terms['Other skills']))*100
     return cv_avg, nlp_avg, trad_ml_avg, others_avg
 
-# Extract text from every page on the file and concatenate in the text variable 
-while count < num_pages:
-    pageObj = pdfReader.getPage(count)
-    count +=1
-    text += pageObj.extractText()
 
-# print("ouput text: ", text)
-
-
-text = text.lower() # Convert all strings to lowercase
-
-print("text in lower case\n\n", text)
-
-
-text = re.sub(r'\d+','',text) # Remove numbers
-
-# print("text after removing the numbers ", text)
+def text_extraction(pdfReader):
+    num_pages = len(pdfReader.pages) # Get total number of pages
+    # print("total number of pages in pdf file: ", num_pages)
+    count = 0 # Initialize a count for the number of pages
+    text = "" # Initialize a text empty string variable
+    # Extract text from every page on the file and concatenate in the text variable 
+    while count < num_pages:
+        pageObj = pdfReader.getPage(count)
+        count +=1
+        text += pageObj.extractText()
+    return text
 
 
-text = text.translate(str.maketrans('','',string.punctuation)) # Remove punctuation
-# print("text after removing punctuation: ", text)
-
-# Create dictionary with industrial and system engineering key terms by area
-terms = {'CV':['opencv', 'tensorflow', 
-               'image classification', 
-               'image segmentation', 
-               'object detection'],      
-        'NLP':['nlp, ''transformer', 
-              'encoder decoder', 
-              'bert', 'text analysis',
-              'sentiment analysis',
-              'chatbot', 'pytorch', 
-              'NLTK', 'sequence model', 
-              'text mining'],
-        'Tradition ML':['python','matplotlib',
-                        'scikit learn', 'regression', 
-                        'numpy', 'pandas'],
-        'Other skills': ['python', 'git', 'docker']
-        }
+def text_processing(text):
+    text = text.lower() # Convert all strings to lowercase
+    print("text in lower case\n\n", text)
+    text = re.sub(r'\d+','',text) # Remove numbers
+    # print("text after removing the numbers ", text)
+    text = text.translate(str.maketrans('','',string.punctuation)) # Remove punctuation
+    # print("text after removing punctuation: ", text)
+    return text
 
 
+def output_calculation(text):
+    # Initializie score counters for each area
+    opencv = 0
+    nlp = 0
+    trad_ml = 0
+    others = 0
 
-# Initializie score counters for each area
-opencv = 0
-nlp = 0
-trad_ml = 0
-others = 0
+    # Obtain the scores for each area
+    for area in terms.keys():
+            
+        if area == 'CV':
+            for word in terms[area]:
+                if word in text:
+                    opencv +=1            
+
+        elif area == 'NLP':
+            for word in terms[area]:
+                if word in text:
+                    nlp +=1
+            
+        elif area == 'Tradition ML':
+            for word in terms[area]:
+                if word in text:
+                    trad_ml +=1        
+            
+        else:        
+            for word in terms[area]:
+                if word in text:
+                    others +=1        
+    cv_avg, nlp_avg, trad_ml_avg, others_avg = average_calculator(opencv, nlp, trad_ml, others, terms)
+    return [cv_avg, nlp_avg, trad_ml_avg, others_avg]
 
 
-scores = [] # Create an empty list where the scores will be stored
+def save_output(avg_res):
+    print("terms.keys(): ",terms.keys())
+    summary = pd.DataFrame(avg_res,index=terms.keys(),columns=['score']).sort_values(by='score',ascending=False)    
+    summary.to_csv("results/resume_summary.csv")
+    print("summary.index", summary.index)
+    # Create pie chart visualization
+    pie = plt.figure(figsize=(10,10))
+    plt.pie(np.array(avg_res)/100, labels=['CV', 'NLP', 'Tradition ML', 'Other skills'], explode = (0.1,0,0,0), autopct='%1.0f%%',shadow=True,startangle=90)
+    plt.title('Machine Learning Engineering Candidate - Resume Decomposition by Areas')
+    plt.axis('equal')
+    plt.show()
+    # Save pie chart as a .png file
+    pie.savefig('results/resume_screening_results.png')
 
-
-# Obtain the scores for each area
-for area in terms.keys():
-        
-    if area == 'CV':
-        for word in terms[area]:
-            if word in text:
-                opencv +=1
-        scores.append(opencv)
-        
-    elif area == 'NLP':
-        for word in terms[area]:
-            if word in text:
-                nlp +=1
-        scores.append(nlp)
-        
-    elif area == 'Tradition ML':
-        for word in terms[area]:
-            if word in text:
-                trad_ml +=1
-        scores.append(trad_ml)
-        
-    else:        
-        for word in terms[area]:
-            if word in text:
-                others +=1
-        scores.append(others)
     
 
+# step1
+fileName = "resume_pdf_files/Arslan_Arif_AI_2021.pdf"
+pdfReader = read_input_pdf(fileName)
 
-cv_avg, nlp_avg, trad_ml_avg, others_avg = average_calculator(opencv, nlp, trad_ml, others, terms)
+# step2
+text = text_extraction(pdfReader)
 
-# Create a data frame with the scores summary
-print("terms.keys(): ",terms.keys())
-summary = pd.DataFrame([cv_avg, nlp_avg, trad_ml_avg, others_avg],index=terms.keys(),columns=['score']).sort_values(by='score',ascending=False)
-summary.to_csv("results/resume_summary.csv")
+# step3
+text = text_processing(text)
 
-avgs = np.array([cv_avg, nlp_avg, trad_ml_avg, others_avg])/100
+# step4
 
+avg_res_list = output_calculation(text)
 
+# step5
 
-print('avgs', avgs)
-
-print("summary.index", summary.index)
-# Create pie chart visualization
-pie = plt.figure(figsize=(10,10))
-
-
-
-plt.pie(avgs, labels=['CV', 'NLP', 'Tradition ML', 'Other skills'], explode = (0.1,0,0,0), autopct='%1.0f%%',shadow=True,startangle=90)
-plt.title('Machine Learning Engineering Candidate - Resume Decomposition by Areas')
-plt.axis('equal')
-plt.show()
-
-# Save pie chart as a .png file
-pie.savefig('results/resume_screening_results.png')
-
-
-# print(opencv,nlp, trad_ml, others)
-
-# print(len(terms['CV']))
+save_output(avg_res_list)
